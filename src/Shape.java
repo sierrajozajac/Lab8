1e6cf6cc97d3bb9ccbd5622394d3a72616a1ffee
SierraJo <Sierra.J.Sallee-1@ou.edu>
2019-03-23 14:40:49 -0500

    added javadoc comments to shape
    added all necessary javadoc comments to the shape enumeration

--- src/Shape.java
+++ src/Shape.java
@@ -1,7 +1,15 @@
+/***
+ * This enumeration represents all shapes necessary for game pieces.
+ * @author Sierra Jo Sallee
+ * @version 03/26/2019
+ */
 public enum Shape {
+	// All enumerations of shapes available
 	THIMBLE, BOOT, RACECAR;
 	
-	@Override
+	/***
+	 * This returns the string representing each of the shape enumerations.
+	 */
 	public String toString() {
 	    switch(this) {
 	      case THIMBLE: return "thimble";
