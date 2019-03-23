b04c87fb7e4048243a488ccd13eb9587d94ecf40
SierraJo <Sierra.J.Sallee-1@ou.edu>
2019-03-23 14:42:19 -0500

    added javadoc comments for location
    added all necessary javadoc comments for the location enumeration

--- src/Location.java
+++ src/Location.java
@@ -1,4 +1,10 @@
+/***
+ * This enumeration represents all locations available on the board game.
+ * @author Sierra Jo Sallee
+ * @version 03/26/3019
+ */
 public enum Location {
+	// All location enumerations available
 	KITCHEN, CONSERVATORY, DINING_ROOM, BALLROOM, STUDY,
 	HALL, LOUNGE, LIBRARY, BILLIARD_ROOM;
 }
